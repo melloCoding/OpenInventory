b8ba914cf7c75386b17ca6739fd9ed9cbdb86d91
Jeffrey Mello <mrjeffreymello@gmail.com>
2025-08-20 19:02:35 -0400

    did a thing with the otehr thing

--- app/app.vue
+++ app/app.vue
@@ -2,5 +2,6 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <button>say hello world</button>
   </div>
 </template>
--- server/plugins/setupScript.ts
+++ server/plugins/setupScript.ts
@@ -1,4 +1,7 @@
+import Database from 'better-sqlite3';
 export default defineNitroPlugin((nitroApp) => {
-    console.log("Started Executing setupScript.ts")
-    
+    console.log("✔ Started Executing setupScript.ts")
+    const Database = require('better-sqlite3');
+    const db = new Database('foobar.db', { verbose: console.log });
+    console.log("✔ Connected to SQLite database")
 })
