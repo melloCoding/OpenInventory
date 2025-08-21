0e75d278347b1c68181fec44bcee7f8948909209
Jeffrey Mello <mrjeffreymello@gmail.com>
2025-08-20 20:20:49 -0400

    FINALLY GOT THE DB TO EXIST

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+onMounted(async () => {
+  // call the server API which runs useDatabaseHere()
+  const res = await $fetch("/api/testdbcreate");
+  console.log("testdbcreate response:", res);
+});
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
--- server/plugins/setupScript.ts
+++ server/plugins/setupScript.ts
@@ -1,7 +1,3 @@
-import Database from 'better-sqlite3';
 export default defineNitroPlugin((nitroApp) => {
-    console.log("✔ Started Executing setupScript.ts")
-    const Database = require('better-sqlite3');
-    const db = new Database('foobar.db', { verbose: console.log });
-    console.log("✔ Connected to SQLite database")
+        
 })
